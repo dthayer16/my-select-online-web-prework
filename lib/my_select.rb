8504acb8fd6c699dde76fcb0a49abc3cc7ba1c5c
David Thayer <dthayer16@gmail.com>
2019-03-05 21:03:55 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   collection2 = []
   while i < collection.length do
-     if yield(collection[i]) 
+     if yield(collection[i])
        collection2 << collection[i]
     i += 1
     end
